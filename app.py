ed3d77dd53041cb2fc35a03071b743d286974b6d
GoatCapricorn <muhammadiskandar1278@gmail.com>
2025-07-11 21:33:26 +0700

    Fix: pakai PORT dari environment variable (Render)

--- app.py
+++ app.py
@@ -75,6 +75,9 @@ def admin():
 
     produk = conn.execute("SELECT * FROM produk").fetchall()
     return render_template('admin.html', produk=produk)
+import os
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
